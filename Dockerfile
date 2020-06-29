b392215e600cb6a7bcb09ebb625258dd84c554d3
Andrew Elder <aelder@audioscience.com>
2020-06-29 10:04:31 -0400

    update to support AudioScience build and FTP operations

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-# Container image that runs your code
-FROM alpine:edge
+# Container image
+FROM amd64/python:2-slim
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
 # Install required packages
-RUN apk add doxygen graphviz ttf-freefont
+RUN apt-get update \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -y doxygen graphviz fonts-freefont-ttf ftp \
+  && rm -rf /var/lib/apt/lists/*
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,50 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ ! -d $2 ]; then
-    echo "Path $2 could not be found!"
+    echo "Path $2 (working-dir) could not be found!"
 fi
+
+if [ ! -d $1 ]; then
+    echo "Path $1 (doxyfile-path) could not be found!"
+fi
+
+if [ ! -d $3 ]; then
+    echo "Path $3 (output-dir) could not be found!"
+fi
+
+if [ ! -d $4 ]; then
+    echo "Path $4 (hpi-dir) could not be found!"
+fi
+
+hpiversion=$(python $4/hpi_version.py {"dotted"})
+
+sed "s!PROJECT_NUMBER.*!PROJECT_NUMBER         = Version_$hpiversion!g;s!OUTPUT_DIRECTORY.*!OUTPUT_DIRECTORY       = $3/dox!g" $1/doxyfile.dox > $3/d.dox
 cd $2
+doxygen $3/d.dox > $3/doxygen.log
+cp stylesheet.css $3/dox/html
+sed -i "s/Modules/Functions/g" $3/dox/html/*.html
 
-if [ ! -f $1 ]; then
-    echo "File $1 could not be found!"
+hpi_version_minor=$(python $4/hpi_version.py {"minor"})
+if (( ${hpi_version_minor} % 2 == 0 ))
+then
+   DOCSUBDIR=html
+else
+   DOCSUBDIR=beta_html
 fi
 
-doxygen $1
+LOCAL_DIR=$3/dox/html
+REMOTE_DIR=internet/download/sdk/$5_usermanual_html/${DOCSUBDIR}
+HOST=ftp.audioscience.com
+ftp -n $HOST <<END_SCRIPT
+quote USER $6
+quote PASS $7
+passive
+cd $REMOTE_DIR
+lcd $LOCAL_DIR
+prompt
+mput *.html
+mput *.png
+mput *.css
+quit
+END_SCRIPT
+
